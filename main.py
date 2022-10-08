8cfa0828f0a33b704d1a8c06d1ce95d666fa66a8
Roop <roopkm7@gmail.com>
2022-10-08 15:25:21 +0530

    Adding class and function

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+class Solution:
+	def sol(self, arr):
+		return self.__helper(arr, len(arr))
+
+	def __helper(self, arr, n):
+		addition = 0
+		for i in arr:
+			addition += i
+
+		return addition
+
 def main():
-	print('This is the main function of this programe. You can do whatever you want')
+	print(f'The sum of array [2, 3, 5, 89, 19] is {Solution().sol([2, 3, 5, 89, 19])}')
 
 if __name__ == '__main__':
 	main()
